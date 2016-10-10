ecb78b4261c4be49d69b85d346f3f1053ec594ba
Peiwen Lu <hi@peiwen.lu>
2016-10-10 09:13:15 +0800

    Organise files structure

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 
 ;; Load path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "packages" user-emacs-directory))
 (require 'init-use-package)
 
 
@@ -21,29 +22,21 @@
 (require 'init-css)
 (require 'init-javascript)
 (require 'init-elixir)
-
 (require 'init-org)
 
 
-;; Custom Functions
+;; My Functions
 (require 'init-my-functions)
+
+
+;; My Packages
 (require 'stylefmt)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters git-timemachine rainbow-mode browse-kill-ring multi-web-mode evil-surround evil-nerd-commenter evil-leader web-beautify alchemist pinyin-search elixir-mode company-quickhelp company-tern tern markdown-mode json-mode js2-mode yaml-mode emmet-mode web-mode buffer-move window-numbering neotree avy smex flycheck evil-magit git-gutter magit evil yasnippet company counsel nyan-mode linum-relative exec-path-from-shell use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Custom File
+(setq custom-file "custom.el")
+(load custom-file)
+
 
 
 (provide 'init)
