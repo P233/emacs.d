23ad0e35990fc6f2b95c52785429ea9610e2f819
P233 <hi@peiwen.lu>
2017-04-09 15:58:57 +0800

    Remove scss-lint

--- lisp/init-css.el
+++ lisp/init-css.el
@@ -5,8 +5,6 @@
 (add-hook 'scss-mode-hook
           (lambda ()
             (setq-local company-backends '(company-files company-dabbrev-code company-dabbrev))
-            ;; (flycheck-select-checker 'scss-lint)
-            ;; (flycheck-mode)
             (local-set-key (kbd "C-x C-s") 'stylefmt-sort-buffer)
             (rainbow-mode)
             (emmet-mode)))
