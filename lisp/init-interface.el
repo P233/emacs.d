ab78a1624c4311879ea70e69312f23cef9c34a80
Peiwen Lu <hi@peiwen.lu>
2017-04-13 17:38:21 +0800

    Add switch-theme function

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -8,24 +8,25 @@
 (scroll-bar-mode -1)
 
 
+;; Show column number
+(column-number-mode)
+
+
+;; Disable blinking cursor
+(blink-cursor-mode -1)
+
+
 ;; Set fonts
 (set-frame-font "PragmataPro 16" t t)
 (set-fontset-font t 'han (font-spec :family "PingFang SC"))
 (mac-auto-operator-composition-mode t) ; Enable ligatures
 
 
-;; Set default farme size
-(setq default-frame-alist '((width  . 200)
-                            (height . 56)
-                            (top    . 0)
-                            (left   . 150)))
-
-
-;; Set theme
-(use-package gruvbox-theme
-  :config
-  (set-face-foreground 'fringe "#665c54")
-  (set-face-foreground 'default "#ebdbb2"))
+;; Enable nyan-cat mode
+(use-package nyan-mode
+  :init
+  (setq nyan-bar-length 24)
+  (nyan-mode))
 
 
 ;; Enable relative line number
@@ -34,24 +35,42 @@
   (setq linum-relative-current-symbol "->"
         linum-relative-format "%2s")
   (global-linum-mode t)
-  (linum-relative-toggle)
-  :config
-  (set-face-attribute 'linum-relative-current-face nil :foreground "#fabd2f" :weight 'normal))
+  (linum-relative-toggle))
 
 
-;; Enable column number
-(column-number-mode)
-
-
-;; Disable blinking cursor
-(blink-cursor-mode -1)
-
-
-;; Enable nyan-cat mode
-(use-package nyan-mode
+;; Set default theme
+(use-package gruvbox-theme
   :init
-  (setq nyan-bar-length 24)
-  (nyan-mode))
+  (load-theme 'gruvbox t)
+  (set-face-foreground 'default "#ebdbb2")
+  (set-face-foreground 'fringe "#665c54")
+  (set-face-attribute 'linum-relative-current-face nil :foreground "#fabd2f" :background "#282828" :weight 'normal))
+
+
+;; Switch theme
+(defvar current-theme-name "gruvbox")
+(defun switch-theme ()
+  (interactive)
+  (if (string-equal current-theme-name "gruvbox")
+      (progn
+        (disable-theme 'gruvbox)
+        (set-face-foreground 'default "#000")
+        (set-face-foreground 'fringe "#7f7f7f")
+        (set-face-attribute 'linum-relative-current-face nil :foreground "#b92028" :background "#f0f0f0" :weight 'normal)
+        (setq current-theme-name "default"))
+    (progn
+      (load-theme 'gruvbox t)
+      (set-face-foreground 'default "#ebdbb2")
+      (set-face-foreground 'fringe "#665c54")
+      (set-face-attribute 'linum-relative-current-face nil :foreground "#fabd2f" :background "#282828" :weight 'normal)
+      (setq current-theme-name "gruvbox"))))
+
+
+;; Set default farme size
+(setq default-frame-alist '((width  . 200)
+                            (height . 56)
+                            (top    . 0)
+                            (left   . 150)))
 
 
 
