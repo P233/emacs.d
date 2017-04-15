224477c495b29c25b53fea98cc0c38f7bec05e86
P233 <hi@peiwen.lu>
2017-04-15 23:27:15 +0800

    Set coding-system to utf-8

--- lisp/init-code-styles.el
+++ lisp/init-code-styles.el
@@ -1,3 +1,11 @@
+;; Prefer utf8
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
 ;; Set standard-indent size
 (setq standard-indent global-indentation-size)
 
