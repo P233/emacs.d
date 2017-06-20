a1631a00f0c466cf738c6d7c07a9a5cb65753029
Peiwen Lu <hi@peiwen.lu>
2017-06-20 15:54:38 +0800

    Update org-mode config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,14 +1,8 @@
-(setq org-startup-folded nil
-      org-startup-truncated nil)
-
-;; enable to export in markdown format
-(require 'ox-md nil t)
-
+(setq org-src-fontify-natively t)
 
 (add-hook 'org-mode-hook
           (lambda ()
-            (modify-syntax-entry ?~ "(~" org-mode-syntax-table)
-            (define-key org-mode-map (kbd "C-'") nil)))
+            (modify-syntax-entry ?~ "(~" org-mode-syntax-table)))
 
 
 
