f168ff14f00e71aaab4144d07ff358dd6e2177b7
Peiwen Lu <hi@peiwen.lu>
2017-06-20 16:06:56 +0800

    Add org-agenda-files

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,8 @@
-(setq org-src-fontify-natively t)
+(load-library "find-lisp")
+
+
+(setq org-src-fontify-natively t
+      org-agenda-files (find-lisp-find-files (expand-file-name "org-notes" user-emacs-directory) "\.org$"))
 
 (add-hook 'org-mode-hook
           (lambda ()
