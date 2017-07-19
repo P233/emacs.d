9675abaa2a30dfb33c9b9a90dc7aa3d5ac452db8
Peiwen Lu <hi@peiwen.lu>
2017-07-19 16:20:20 +0800

    Remove nlinum-relative mode

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -40,14 +40,6 @@
   (nyan-mode))
 
 
-;; Relative line number
-(use-package nlinum-relative
-  :config
-  (setq nlinum-relative-redisplay-delay 0
-        nlinum-relative-current-symbol "->")
-  (add-hook 'prog-mode-hook 'nlinum-relative-mode))
-
-
 ;; Set default theme
 (use-package gruvbox-theme
   :init
