a722e8d623a067ef84d0f1f2a50c6ca3611c56d6
Peiwen Lu <hi@peiwen.lu>
2018-02-06 16:55:15 +0800

    Remove submodule

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,9 +1,3 @@
-(load-library "find-lisp")
-
-
-(setq org-src-fontify-natively t
-      org-agenda-files (find-lisp-find-files (expand-file-name "org-notes" user-emacs-directory) "\.org$"))
-
 (add-hook 'org-mode-hook
           (lambda ()
             (modify-syntax-entry ?~ "(~" org-mode-syntax-table)))
