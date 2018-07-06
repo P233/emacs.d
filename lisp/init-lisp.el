8386558a1c7f7ee61496bc7d4206a63496bc1a32
Peiwen Lu <hi@peiwen.lu>
2018-07-06 17:01:28 +0800

    Add lispy mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,7 +1,10 @@
+;; Lispy
+(use-package lispy)
+
 ;; Parinfer mode
 (use-package parinfer
   :init
-  (setq parinfer-extensions '(defaults pretty-parens smart-tab smart-yank)
+  (setq parinfer-extensions '(defaults pretty-parens lispy smart-tab smart-yank)
         parinfer-auto-switch-indent-mode t)
   :config
   (add-hook 'clojure-mode-hook 'parinfer-mode)
