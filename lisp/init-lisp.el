345c39ab5259bd4fea11251e08247cdddf3d911c
Peiwen Lu <hi@peiwen.lu>
2018-07-08 15:13:29 +0800

    Update parinfer settings

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -4,8 +4,7 @@
 ;; Parinfer mode
 (use-package parinfer
   :init
-  (setq parinfer-extensions '(defaults pretty-parens lispy smart-tab smart-yank)
-        parinfer-auto-switch-indent-mode t)
+  (setq parinfer-extensions '(defaults pretty-parens lispy smart-tab smart-yank))
   :config
   (add-hook 'clojure-mode-hook 'parinfer-mode)
   (add-hook 'emacs-lisp-mode-hook 'parinfer-mode))
