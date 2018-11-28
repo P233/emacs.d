ba0692a78961c2b7dd3960018222c823323f7f35
Peiwen Lu <hi@peiwen.lu>
2018-11-28 23:05:17 +0800

    Enable emmet scss syntax

--- lisp/init-css.el
+++ lisp/init-css.el
@@ -3,13 +3,18 @@
 (use-package postcss-sorting)
 
 (setq css-indent-offset global-indentation-size)
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
 (add-hook 'css-mode-hook
           (lambda ()
             (setq-local company-backends '(company-files (company-css :with company-dabbrev company-dabbrev-code)))
             (add-hook 'before-save-hook 'postcss-sorting-buffer nil 'local)
             (rainbow-mode)
             (emmet-mode)))
+
+
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (setq-local emmet-use-scss-syntax t)))
 
 
 
