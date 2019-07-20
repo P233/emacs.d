7720d8be83b037c40e974b9300c43e8eb06cd809
Peiwen Lu <hi@peiwen.lu>
2019-07-20 10:12:28 +0800

    Remove clojure mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -7,18 +7,7 @@
   :custom
   (parinfer-extensions '(defaults pretty-parens lispy smart-tab smart-yank))
   :config
-  (add-hook 'clojure-mode-hook #'parinfer-mode)
   (add-hook 'emacs-lisp-mode-hook #'parinfer-mode))
-
-
-;; Clojure and ClojureScript mode
-(use-package clojure-mode
-  :custom
-  (clojure-indent-style :align-arguments))
-
-
-;; Cider Repl
-(use-package cider)
 
 
 
