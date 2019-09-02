4d346942961a89590d9d62a1cff33c34c174d496
Peiwen Lu <hi@peiwen.lu>
2019-09-02 14:20:06 +0800

    Add gcmh mode

--- lisp/init-settings.el
+++ lisp/init-settings.el
@@ -1,7 +1,7 @@
-;; Setup garbage collection
-;; https://emacs.stackexchange.com/questions/34342/is-there-any-downside-to-setting-gc-cons-threshold-very-high-and-collecting-ga
-(setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
-(run-with-idle-timer 2 t (lambda () (garbage-collect)))
+;; http://akrl.sdf.org/
+(use-package gcmh
+  :config
+  (gcmh-mode))
 
 
 ;; Disable backup, auto-save and lockfiles
