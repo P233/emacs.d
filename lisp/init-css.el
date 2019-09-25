22020af62bace1e82fa39236b1b96885609d6385
Peiwen Lu <hi@peiwen.lu>
2019-09-25 09:50:42 +0800

    Add prettier-js to css-mode before-save-hook

--- lisp/init-css.el
+++ lisp/init-css.el
@@ -1,10 +1,14 @@
 (use-package postcss-sorting)
 
+(defun my/css-before-save-hook ()
+  (postcss-sorting-buffer)
+  (prettier-js))
+
 (setq css-indent-offset global-indentation-size)
 (add-hook 'css-mode-hook
           (lambda ()
             (setq-local company-backends '(company-css company-tabnine))
-            (add-hook 'before-save-hook 'postcss-sorting-buffer nil 'local)
+            (add-hook 'before-save-hook 'my/css-before-save-hook nil 'local)
             (emmet-mode)))
 
 
