01b912975a48b32662cd2233faa688bf1925da4b
Peiwen Lu <hi@peiwen.lu>
2019-12-26 14:23:35 +0800

    Add doom-modeline

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -46,25 +46,24 @@
   (window-numbering-mode))
 
 
+(use-package doom-modeline
+  :custom
+  (doom-modeline-icon nil)
+  :hook
+  (after-init . doom-modeline-mode))
+
+
 ;; Gruvbox Theme
 (defun my/load-daytime-theme ()
   "Load gruvbox-light-soft theme."
   (interactive)
   (load-theme 'gruvbox-light-soft t)
-  (set-face-foreground 'window-numbering-face "#AF3A03")
-  (set-face-foreground 'mode-line-buffer-id "#AF3A03")
-  (set-face-foreground 'vc-up-to-date-state "#8EC07C")
-  (set-face-foreground 'vc-edited-state "#005F87")
-  (set-face-foreground 'vc-locally-added-state "#B57614")
-  (set-face-foreground 'vc-conflict-state "#9D0006)")
   (set-face-attribute 'mode-line nil :foreground "#427B58" :background "#FAF0C9"))
 
 (defun my/load-nighttime-theme ()
   "Load gruvbox-dark-hard theme."
   (interactive)
   (load-theme 'gruvbox-dark-hard t)
-  (set-face-foreground 'window-numbering-face "#FB4934")
-  (set-face-foreground 'mode-line-buffer-id "#FB4934")
   (set-face-attribute 'mode-line nil :background "#282828"))
 
 (use-package gruvbox-theme
