e08810da7c0c4595b452cdbc2ecbbacd4fc97177
Peiwen Lu <hi@peiwen.lu>
2020-04-07 10:29:12 +0800

    Enable stylelint for scss files

--- lisp/init-css.el
+++ lisp/init-css.el
@@ -10,5 +10,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
 
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (flycheck-mode)
+            (flycheck-select-checker 'scss-stylelint)))
+
 
 (provide 'init-css)
