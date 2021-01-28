f228e6cb3c235a577bb6257c9e212f63f5eb0278
Peiwen Lu <hi@peiwen.lu>
2021-01-28 20:53:42 +0800

    Update web-mode-content-types-alist

--- lisp/init-markup.el
+++ lisp/init-markup.el
@@ -3,6 +3,8 @@
   ("\\.html\\'" "\\.[jt]sx?\\'" "\\.vue\\'" "\\.svelte\\'" "\\.twig\\'")
   :init
   (add-to-list 'magic-mode-alist '("import.*react" . web-mode))
+  (setq web-mode-content-types-alist '(("jsx" . "\\.[jt]sx?\\'")
+                                       ("vue" . "\\.vue\\'")))
   :custom
   (web-mode-block-padding my/indentation-size)
   (web-mode-style-padding my/indentation-size)
@@ -10,8 +12,6 @@
   (web-mode-attr-indent-offset my/indentation-size)
   (web-mode-attr-value-indent-offset my/indentation-size)
   (web-mode-enable-current-element-highlight t)
-  (web-mode-content-types-alist '(("jsx" . "\\.[jt]sx?\\'")
-                                  ("vue" . "\\.vue\\'")))
   :config
   (set-face-background 'web-mode-current-element-highlight-face "#AF3A03")
   :hook
