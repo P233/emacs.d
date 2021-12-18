2e5cfeba49b05b890d0e89ef61f062cc29509421
Peiwen Lu <hi@peiwen.lu>
2021-12-18 21:12:39 +0800

    Replace parinfer-rust-mode with paredit-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,12 +1,10 @@
 ;; -*- lexical-binding: t; -*-
 (use-package lispy :defer t)
 
-(use-package parinfer-rust-mode
+(use-package paredit
   :defer t
-  :config
-  (setq parinfer-rust-auto-download t)
   :hook
-  ((emacs-lisp-mode clojure-mode) . parinfer-rust-mode))
+  ((emacs-lisp-mode clojure-mode) . paredit-mode))
 
 (use-package clojure-mode :defer t)
 
