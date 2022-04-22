fbf0b6dfedf7b1be6e6bf4ffe51763e027778b5f
Peiwen Lu <hi@peiwen.lu>
2022-04-22 21:47:35 +0800

    ivy highlight org files

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -12,7 +12,8 @@
         ivy-height 20
         ivy-use-virtual-buffers t
         ivy-use-selectable-prompt t
-        enable-recursive-minibuffers t)
+        enable-recursive-minibuffers t
+        ivy-switch-buffer-faces-alist '((dired-mode . ivy-subdir) (org-mode . link)))
   (ivy-mode 1)
   :bind
   (("M-x"     . counsel-M-x)
