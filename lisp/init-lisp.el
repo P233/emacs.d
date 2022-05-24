216bd9ed35de06a1ea0e468740943963469dd469
Peiwen Lu <hi@peiwen.lu>
2022-05-24 12:59:23 +0800

    Use lispy instead of paredit

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,17 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 (use-package lispy
-  :defer t)
-
-(use-package paredit
   :defer t
   :hook
-  ((emacs-lisp-mode clojure-mode) . paredit-mode))
+  ((emacs-lisp-mode clojure-mode) . lispy-mode))
 
 (use-package clojure-mode
   :defer t)
 
 (use-package cider
-  :defer t
   :after clojure-mode)
 
 
