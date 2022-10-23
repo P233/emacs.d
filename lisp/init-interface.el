0d4a4b42afceaea44d3f4c73e702f53f00c6ec44
Peiwen Lu <hi@peiwen.lu>
2022-10-23 17:20:42 +0800

    Update modus-themes settings

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -17,6 +17,8 @@
   :init
   (setq modus-themes-bold-constructs nil
         modus-themes-italic-constructs t
+        modus-themes-fringes nil
+        modus-themes-hl-line nil
         modus-themes-org-blocks 'tinted-background
         modus-themes-region '(bg-only no-extend)
         modus-themes-mode-line '(accented borderless)
