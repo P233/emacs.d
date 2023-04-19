ce726cd5297eb4f55db62c3f8fd37f2f364931a8
Peiwen Lu <hi@peiwen.lu>
2023-04-19 22:14:39 +0800

    Add web-mode for html and astro files

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -35,5 +35,21 @@
   :hook
   ((css-mode js-ts-mode typescript-ts-mode tsx-ts-mode json-ts-mode) . prettier-js-mode))
 
+(use-package web-mode
+  :mode
+  ("\\.html\\'" "\\.astro\\'")
+  :custom
+  (web-mode-block-padding my/indentation-size)
+  (web-mode-style-padding my/indentation-size)
+  (web-mode-script-padding my/indentation-size)
+  (web-mode-attr-indent-offset my/indentation-size)
+  (web-mode-attr-value-indent-offset my/indentation-size)
+  (web-mode-enable-auto-indentation nil)
+  (web-mode-enable-current-element-highlight t)
+  :custom-face
+  (web-mode-html-tag-face ((t (:inherit font-lock-function-name-face :foreground unspecified))))
+  (web-mode-html-attr-name-face ((t (:inherit font-lock-type-face :foreground unspecified))))
+  (web-mode-current-element-highlight-face ((t (:background "#3f6faf")))))
+
 
 (provide 'init-web)
