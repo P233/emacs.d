62e61590bc38fabb03d762af411f89c631c6ee75
Peiwen Lu <hi@peiwen.lu>
2023-10-23 13:56:53 +0800

    Update swift configs

--- lisp/init-swift.el
+++ lisp/init-swift.el
@@ -2,14 +2,8 @@
 (use-package swift-mode
   :defer t
   :custom
+  (swift-mode:multiline-statement-offset 0)
   (swift-mode:basic-offset my/indentation-size))
-
-(use-package swift-format
-  :straight (:type git :host github :repo "p233/swift-format.el")
-  :after swift-mode
-  :hook
-  (swift-mode . (lambda ()
-                  (add-hook 'before-save-hook 'swift-format-buffer nil t))))
 
 
 (provide 'init-swift)
