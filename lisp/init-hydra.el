f1a6d8a91c55bb0863350c2c84d3ead3fad6cb25
Peiwen Lu <hi@peiwen.lu>
2023-11-10 11:43:19 +0800

    Update avy hydra

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -1,15 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 (use-package hydra)
 
-(defhydra hydra-avy (:columns 3)
-  "Avy Editing"
-  ("cl" avy-copy-line "copy-line")
-  ("kl" avy-kill-whole-line "kill-line")
-  ("ml" avy-move-line "move-line")
-  ("cr" avy-copy-region "copy-region")
-  ("kr" avy-kill-region "kill-region")
-  ("mr" avy-move-region "move-region"))
-(global-set-key (kbd "C-.") 'hydra-avy/body)
+(defhydra hydra-avy (:color blue :columns 3)
+  "Avy Hydra"
+  ("c" avy-copy-line "copy-line")
+  ("k" avy-kill-whole-line "kill-line")
+  ("m" avy-move-line "move-line")
+  ("C" avy-copy-region "copy-region")
+  ("K" avy-kill-region "kill-region")
+  ("M" avy-move-region "move-region"))
+(global-set-key (kbd "C-c C-g") 'hydra-avy/body)
 
 
 
