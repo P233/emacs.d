8f0635552bdf6cec5cd9c894e2190820f264ca1e
Peiwen Lu <hi@peiwen.lu>
2023-11-15 21:37:01 +0800

    Unbind "C-c C-f" in elisp-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,4 +1,9 @@
 ;; -*- lexical-binding: t; -*-
+(use-package elisp-mode
+  :straight (:type built-in)
+  :config
+  (unbind-key "C-c C-f" emacs-lisp-mode-map))
+
 (use-package paredit
   :hook
   (emacs-lisp-mode . paredit-mode))
