ec3dc3a8ff8b04ab123a65f5c82abdd8e2a1b06b
Peiwen Lu <hi@peiwen.lu>
2023-12-15 21:53:53 +0800

    Setup org-mode

--- lisp/init-note.el
+++ lisp/init-note.el
@@ -9,6 +9,53 @@
   (fixed-pitch ((t (:font "PragmataPro Mono" :height 1.0))))
   (variable-pitch ((t (:font "Noto Serif" :height 1.0)))))
 
+(use-package org
+  :straight (:type built-in)
+  :defer t
+  :custom
+  (org-ellipsis " ⭍")
+  (org-pretty-entities t)
+  (org-startup-indented t)
+  (org-hide-leading-stars t)
+  (org-hide-emphasis-markers t)
+  (org-image-actual-width '(800))
+  (org-startup-with-inline-images t)
+  :custom-face
+  (org-level-1 ((t (:height 1.15))))
+  (org-level-2 ((t (:height 1.13))))
+  (org-level-3 ((t (:height 1.11))))
+  (org-level-4 ((t (:height 1.09))))
+  (org-level-5 ((t (:height 1.07))))
+  (org-level-6 ((t (:height 1.05))))
+  (org-level-7 ((t (:height 1.03))))
+  (org-level-8 ((t (:height 1.01))))
+  (org-date ((t (:inherit 'fixed-pitch))))
+  (org-todo ((t (:inherit 'fixed-pitch))))
+  (org-done ((t (:inherit 'fixed-pitch))))
+  (org-drawer ((t (:inherit 'fixed-pitch))))
+  (org-ellipsis ((t (:inherit 'fixed-pitch))))
+  (org-property-value ((t (:inherit 'fixed-pitch))))
+  (org-special-keyword ((t (:inherit 'fixed-pitch))))
+  (org-headline-done ((t (:inherit 'variable-pitch))))
+  :config
+  (unbind-key "C-'" org-mode-map)
+  :hook
+  (org-mode . (lambda ()
+                (setq-local line-spacing 2)
+                (visual-line-mode)
+                (visual-fill-column-mode)
+                (mixed-pitch-mode)
+                (org-superstar-mode))))
+
+(use-package org-superstar
+  :defer t
+  :custom
+  (org-superstar-headline-bullets-list '("☰" "☱" "☲" "☳" "☴" "☵" "☶" "☷"))
+  (org-superstar-item-bullet-alist '((43 . "⬧") (45 . "⬨")))
+  :custom-face
+  (org-superstar-item ((t (:inherit 'fixed-pitch))))
+  (org-superstar-header-bullet ((t (:height 232 :inherit 'fixed-pitch)))))
+
 (use-package markdown-mode
   :custom-face
   (markdown-header-face-1 ((t (:height 1.25))))
