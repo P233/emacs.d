37ab36f3526cff90379d2a259aa5e6165f11ebc0
Peiwen Lu <hi@peiwen.lu>
2024-02-12 20:17:55 +0800

    Update swift mode settings

--- lisp/init-swift.el
+++ lisp/init-swift.el
@@ -1,9 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 (use-package swift-mode
-  :defer t
-  :custom
-  (swift-mode:basic-offset 2)
-  (swift-mode:multiline-statement-offset 0))
+  :defer t)
 
 
 (provide 'init-swift)
