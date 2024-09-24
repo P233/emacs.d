0e300a06792977b91a2dac4d485fa830adc37f42
Peiwen Lu <hi@peiwen.lu>
2024-09-24 20:37:18 +0800

    Enable yaml-ts-mode

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -5,6 +5,7 @@
 (add-to-list 'auto-mode-alist '("\\.tsx$" . tsx-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(c\\|m\\)?ts$" . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(c\\|m\\)?jsx?$" . js-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-ts-mode))
 
 (use-package glsl-mode
   :straight (:type git :host github :repo "jimhourihan/glsl-mode"))
