41a5762eedafa53caac11753a62c7333c14469d9
Peiwen Lu <hi@peiwen.lu>
2025-07-16 00:46:34 +0800

    Update treemacs configs

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -49,11 +49,21 @@
 (use-package treemacs
   :custom
   (treemacs-width 28)
+  (treemacs-text-scale nil)
   (treemacs-no-png-images t)
+  (treemacs-show-hidden-files nil)
+  :init
+  (defun my/treemacs-ignore-files (_ absolute-path)
+    (or (string-suffix-p "dist" absolute-path)
+        (string-suffix-p "node_modules" absolute-path)))
+  :config
+  (treemacs-project-follow-mode t)
+  (add-to-list 'treemacs-ignored-file-predicates #'my/treemacs-ignore-files)
   :custom-face
   (treemacs-root-face ((t (:height 1.1))))
   :bind
-  ("<f3>" . treemacs))
+  ("<f3>" . treemacs)
+  ("M-<f3>" . treemacs-select-window))
 
 
 ;; https://emacs-china.org/t/topic/945/2
